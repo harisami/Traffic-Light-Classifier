2fb4ac76826ab2ad7d0c8a3ec21333eb4f8ca4c2
Haris Sami <harisami.90@gmail.com>
2019-10-27 18:33:27 -0400

    project commit

---
+++ helpers.py
@@ -0,0 +1,50 @@
+# Helper functions
+
+import os
+import glob # library for loading images from a directory
+import matplotlib.image as mpimg
+import numpy as np
+
+
+
+# This function loads in images and their labels and places them in a list
+# The list contains all images and their associated labels
+# For example, after data is loaded, im_list[0][:] will be the first image-label pair in the list
+def load_dataset(image_dir):
+    
+    # Populate this empty image list
+    im_list = []
+    image_types = ["red", "yellow", "green"]
+    
+    # Iterate through each color folder
+    for im_type in image_types:
+        
+        # Iterate through each image file in each image_type folder
+        # glob reads in any image with the extension "image_dir/im_type/*"
+        for file in glob.glob(os.path.join(image_dir, im_type, "*")):
+            
+            # Read in the image
+            im = mpimg.imread(file)
+            
+            # Check if the image exists/if it's been correctly read-in
+            if not im is None:
+                # Append the image, and it's type (red, green, yellow) to the image list
+                im_list.append((im, im_type))
+
+    return im_list
+
+def avg_brightness(rgb_image):
+    
+    hsv = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)
+    h = hsv[:,:,0]
+    s = hsv[:,:,1]
+    v = hsv[:,:,2]
+    
+    avg = 0
+    
+    sum_brightness = np.sum(v)
+    area = rgb_image.shape[0] * rgb_image.shape[1]
+    
+    avg = sum_brightness / area
+    
+    return avg
